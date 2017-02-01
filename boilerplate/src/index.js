bca1f32654a0c5e671a1d3d6809a7f5e238653e9
G. Grau <ggrau@me.com>
2017-02-01 17:15:03 +0100

    Add boilerplate template

---
+++ boilerplate/src/index.js
@@ -0,0 +1,17 @@
+/*!
+ * boilerplate
+ *
+ * Module one-liner.
+ *
+ * @copyright Guillermo Grau Panea 2016
+ * @license MIT
+ */
+// @flow
+
+// -- Example documentation that will be included in the README.md file:
+// --
+// -- **foo()**
+// -- Usage: foo(n: number): number
+export function foo(n: number): number {
+  return n * 2;
+}
---
+++ boilerplate/test/test.js
@@ -0,0 +1,6 @@
+import test from 'ava';
+import * as m from '../lib/index';
+
+test('foo', t => {
+  t.deepEqual(m.foo(2), 4);
+});
