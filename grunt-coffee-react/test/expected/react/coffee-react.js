4dd08e4c60a76a79f60ba4ef1a517422951e4d55
G. Grau <ggrau@me.com>
2017-02-01 17:12:00 +0100

    Add grunt-coffee-react experiments

---
+++ grunt-coffee-react/test/expected/react/coffee-react.js
@@ -0,0 +1,19 @@
+(function() {
+  /** @jsx React.DOM */;
+  var Car;
+
+  Car = React.createClass({
+    render: function() {
+      var ref;
+      return React.createElement(Vehicle, {
+        "doors": 4.,
+        "locked": isLocked(),
+        "data-colour": "red",
+        "on": true
+      }, React.createElement(Parts.FrontSeat, null), React.createElement(Parts.BackSeat, null), React.createElement("p", {
+        "className": "kickin"
+      }, "Which seat can I take? ", ((ref = this.props) != null ? ref.seat : void 0) || 'none'));
+    }
+  });
+
+}).call(this);
