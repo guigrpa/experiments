3cf6caeaf49c319e10ceb34a68ca3c96712948f1
G. Grau <ggrau@me.com>
2017-08-01 16:39:33 +0200

    Add animation

--- svg-animations/pages/index.js
+++ svg-animations/pages/index.js
@@ -4,14 +4,24 @@ class AnimatedPath extends React.Component {
       <div>
         <svg width={500} height={500}>
           <rect x={10} y={20} width={10} height={40} />
-          <path d="M10,10L200,200" />
+          <path d="M30,10L230,10" />
         </svg>
         <style jsx>{`
           rect {
             fill: blue;
           }
           path {
-            stroke: red;
+            stroke: gray;
+            stroke-width: 20;
+            stroke-dasharray: 200;
+            stroke-dashoffset: 200;
+            stroke-linecap: round;
+            animation: dash 2s linear forwards infinite;
+          }
+          @keyframes dash {
+            to {
+              stroke-dashoffset: 0;
+            }
           }
         `}</style>
       </div>
