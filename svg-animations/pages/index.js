fbf3ac72664fa58c5e46971462423e07cb7a5ee6
G. Grau <ggrau@indra.es>
2017-09-04 12:49:20 +0200

    Minor

--- svg-animations/pages/index.js
+++ svg-animations/pages/index.js
@@ -4,7 +4,7 @@ class AnimatedPath extends React.Component {
       <div>
         <svg width={500} height={500}>
           <rect x={10} y={20} width={10} height={40} />
-          <path d="M10,10L200,200" />
+          <path d="M10,10L310,10" />
         </svg>
         <style jsx>{`
           rect {
@@ -12,6 +12,14 @@ class AnimatedPath extends React.Component {
           }
           path {
             stroke: red;
+            stroke-dasharray: 300;
+            stroke-dashoffset: 300;
+            animation: dash 2s linear forwards;
+          }
+          @keyframes dash {
+            to {
+              stroke-dashoffset: 250;
+            }
           }
         `}</style>
       </div>
