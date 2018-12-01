185a7be9e12b7529f51935d490d524a5d56543b3
G. Grau <guigrpa@gmail.com>
2018-12-01 23:29:41 +0100

    Minor

--- ts/src/foo.ts
+++ ts/src/foo.ts
@@ -1,3 +1,4 @@
 export const sum = (...a: number[]) => a.reduce((acc, val) => acc + val, 0);
 
+// This is an error, obviously -- we introduce it to make sure Jest does not show a type error
 sum('a');
